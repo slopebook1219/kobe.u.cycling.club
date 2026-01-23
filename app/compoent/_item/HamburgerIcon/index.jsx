24bc19fe4cf3a5699b8da5e10ec76d0e2500ebe2
坂本大幹 <saka.ttt.1219@gmail.com>
2026-01-23 23:01:38 +0900

    ハンバーガーアイコンの作成

---
+++ app/compoent/_item/HamburgerIcon/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export default function HamburgerIcon({ onClick, isOpen }) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex h-10 w-10 flex-col items-center justify-center space-y-2"
+    >
+      <span
+        className={`block h-[1px] w-8 rounded-full bg-gray-500 transition-transform duration-300 ease-in-out
+          ${isOpen ? "w-9 rotate-45 translate-y-2 bg-white" : ""}
+        `}
+      ></span>
+      <span
+        className={`block h-[1px] w-8 rounded-full bg-gray-500 transition-opacity duration-300 ease-in-out
+          ${isOpen ? "opacity-0" : ""}
+        `}
+      ></span>
+      <span
+        className={`block h-[1px] w-8 rounded-full bg-gray-500 transition-transform duration-300 ease-in-out
+          ${isOpen ? "w-9 -rotate-45 -translate-y-[10px] bg-white" : ""}
+        `}
+      ></span>
+    </button>
+  );
+}
---
+++ app/compoent/_item/HeaderWithMenu/index.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import HamburgerIcon from '../HamburgerIcon';
+
+export default function HeaderWithMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <div className="fixed top-7 right-7 z-50">
+        <HamburgerIcon isOpen={isOpen} onClick={handleMenuToggle} />
+      </div>
+      <div
+        className={`
+          fixed inset-0 z-30
+          transition-opacity duration-1000 ease-in-out
+          ${isOpen ? 'opacity-40 bg-black' : 'opacity-0 pointer-events-none'}
+        `}
+        onClick={handleMenuToggle}
+      ></div>
+      <div
+        className={`
+          fixed inset-y-0 right-0 md:w-64 w-35 text-white z-40
+          transform transition-transform duration-500 ease-in-out
+          ${isOpen ? 'translate-x-0' : 'translate-x-full'}
+        `}
+      >
+        <nav className="pt-20 ">
+          <ul>
+            <li className="md:py-4 py-2 md:px-4 cursor-pointer text-2xl md:text-3xl">
+              <Link href="/" onClick={handleMenuToggle}>
+                TOP
+              </Link>
+            </li>
+            <li className="md:py-4 py-2 md:px-4 cursor-pointer text-2xl md:text-3xl">
+              <Link href="/member" onClick={handleMenuToggle}>
+                メンバー紹介
+              </Link>
+            </li>
+            <li className="md:py-4 py-2 md:px-4 cursor-pointer text-2xl md:text-3xl">
+              <Link href="/album" onClick={handleMenuToggle}>
+                アルバム
+              </Link>
+            </li>
+            <li className="md:py-4 py-2 md:px-4 cursor-pointer text-2xl md:text-3xl">
+              <Link
+                href="https://www.instagram.com/kobecycle_gram?igsh=a2V3eXRyZjRteWYy"
+                onClick={handleMenuToggle}
+              >
+                instaはこちら
+              </Link>
+            </li>
+            <li className="md:py-4 py-2 md:px-4 cursor-pointer text-2xl md:text-3xl">
+              <Link href="https://x.com/kobe_u_cycling" onClick={handleMenuToggle}>
+                Xはこちら
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+}
