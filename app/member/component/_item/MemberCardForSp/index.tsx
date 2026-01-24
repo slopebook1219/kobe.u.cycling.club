b3b20d8dab8fe3d08f56fe436dbd28f0b65fc9ce
坂本大幹 <saka.ttt.1219@gmail.com>
2026-01-24 17:41:33 +0900

    メンバーページのデザイン変更

---
+++ app/member/component/_item/MemberCardForSp/index.tsx
@@ -0,0 +1,36 @@
+import { Member } from '@/app/member/component/_type';
+import Link from 'next/link';
+import Image from 'next/image';
+export type Props = {
+  results: Member[];
+};
+export function MemberCardForSp({ results }: Props) {
+  return (
+    <section className="max-w-6xl mx-auto py-12 px-2">
+      <div className="mb-10 text-center">
+        <h2 className="text-2xl font-bold mb-3">メンバー紹介</h2>
+        <p className="text-gray-600 leading-relaxed">
+          私たちは現在<strong>{results.length}名</strong>で活動しています。
+          <br />
+          学部・学年を越えて、それぞれの関心や強みを活かしながら、
+          日々さまざまな活動に取り組んでいます。
+        </p>
+      </div>
+      <div className="grid grid-cols-2 gap-4">
+        {results.map((member) => (
+          <Link key={member.id} href={`/member/${member.id}`} className="block">
+            <div className="overflow-hidden border border-gray-300 bg-white hover:bg-gray-50 transition">
+              <div className="relative aspect-square">
+                <Image src={member.image.url} alt={member.name} fill className="object-cover" />
+              </div>
+              <div className="text-center">
+                <p className="text-sm text-gray-500">{member.faculty}</p>
+                <h3 className="mt-1 text-md font-semibold">{member.name}</h3>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+}
--- app/member/presentational/index.tsx
+++ app/member/presentational/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import type { Member } from '@/app/member/component/_type';
+import { MemberCard } from '@/app/member/component/_item/MemberCard';
+import { MemberCardForSp } from '@/app/member/component/_item/MemberCardForSp';
 
 export type Props = {
   results: Member[];
@@ -8,31 +10,13 @@ export type Props = {
 
 export default function PagePresentational({ results }: Props) {
   return (
-    <section className="max-w-6xl mx-auto px-4 py-14">
-      <div className="mb-10 text-center">
-        <h2 className="text-2xl font-bold mb-3">メンバー紹介</h2>
-        <p className="text-gray-600 leading-relaxed">
-          私たちは現在<strong>{results.length}名</strong>で活動しています。
-          <br />
-          学部・学年を越えて、それぞれの関心や強みを活かしながら、
-          日々さまざまな活動に取り組んでいます。
-        </p>
+    <>
+      <div className="hidden md:block">
+        <MemberCard results={results} />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 p-6">
-        {results.map((member) => (
-          <Link key={member.id} href={`/member/${member.id}`} className="block">
-            <div className="overflow-hidden border border-gray-300 bg-white hover:bg-gray-50 transition">
-              <div className="relative aspect-square">
-                <Image src={member.image.url} alt={member.name} fill className="object-cover p-5" />
-              </div>
-              <div className="text-center">
-                <p className="text-sm text-gray-500">所属：{member.faculty}</p>
-                <h3 className="mt-1 text-lg font-semibold mb-4">名前：{member.name}</h3>
-              </div>
-            </div>
-          </Link>
-        ))}
+      <div className="block md:hidden">
+        <MemberCardForSp results={results} />
       </div>
-    </section>
+    </>
   );
 }
