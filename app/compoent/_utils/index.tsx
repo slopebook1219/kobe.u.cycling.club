c0f51d037c8a84e141ae542e63f77cb1622d60e7
坂本大幹 <saka.ttt.1219@gmail.com>
2026-01-24 22:43:54 +0900

    タイムゾーン調整

--- app/compoent/_utils/index.tsx
+++ app/compoent/_utils/index.tsx
@@ -1,5 +1,6 @@
 export function formatDate(date: string) {
   return new Date(date).toLocaleDateString('ja-JP', {
+    timeZone: 'Asia/Tokyo',
     year: 'numeric',
     month: 'long',
     day: 'numeric',
