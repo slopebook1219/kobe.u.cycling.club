a0da4b2e73eb4fa13864bca374d81ff3dc8e51cd
坂本大幹 <saka.ttt.1219@gmail.com>
2026-04-11 11:18:06 +0900

    スライド作成

--- app/compoent/_actions/index.tsx
+++ app/compoent/_actions/index.tsx
@@ -2,10 +2,7 @@ import { client } from '@/lib/microcms';
 export async function fetchTopImage() {
   const data = await client.get({
     endpoint: 'top',
-    queries: {
-      fields: 'image',
-      limit: 100,
-    },
+    queries: { limit: 100 },
   });
   return data.contents;
 }
